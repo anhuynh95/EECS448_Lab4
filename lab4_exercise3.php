a372bcdf9bcbadec2aab7807505989e736b60f06
An Huynh <arizhuynh211@gmail.com>
2018-09-30 19:55:50 -0500

    Updated

--- lab4_exercise3.php
+++ lab4_exercise3.php
@@ -1,23 +1,28 @@
 <?php
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
 	echo "<link rel='stylesheet' type='text/css' href='lab4_exercise3.css'/>";  
 	echo("<p> Welcome to Random Shop </p>");
 	echo("<p>Username: ". $_GET["username"] ."</p>");
 	echo("<p>Password: ". $_GET["password"] ."</p><br>");
 
 	$u1 = $_GET["unit1"];
+	echo number_format($u1, 2);
 	$c1 = 799.99;
 	$s1 = $u1*$c1;
 	$u2 = $_GET["unit2"];
+	echo number_format($u1, 2);
 	$c2 = 1999.00;
 	$s2 = $u2*$c2;
 	$u3 = $_GET["unit3"];
+	echo number_format($u1, 2);
 	$c3 = 119.99;
 	$s3 = $u3*$c3;
 
 	$ship = $_GET["shipping"];
 	$total = $s1 + $s2 + $s3 +$ship;
 
-	echo ("<table class='table1'>");
+	echo ("<table>");
 	echo("<p>Here is Your Receipt</p>");
 		echo ("<tr> \n");
 			echo ("<th></th>");
